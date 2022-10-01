0e8563b9d44f0f6bc6b032297504b5c232e3bc27
mdschuster <micahdschuster@gmail.com>
2022-10-01 15:45:37 -0400

    Add formations to spawn

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -11,6 +11,9 @@ public class SpawnManager : MonoBehaviour
 
     public ObjectPooler baseEnemyPool;
 
+    [Header("Formations")] 
+    public GameObject[] formations;
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,14 +26,50 @@ public class SpawnManager : MonoBehaviour
     {
         if (spawnTimer <= 0f)
         {
-            spawnEnemy();
+            float spawnRandom = Random.Range(0f, 1f);
+            if (spawnRandom <= 1.0f)
+            {
+                print("Spawn Formation");
+                spawnFormation();
+            }
+            else
+            {
+                spawnSingleEnemy();
+            }
+
             spawnTimer = timeBetweenSpawns + Random.Range(-spawnTimeVariation, spawnTimeVariation);
         }
 
         spawnTimer -= Time.deltaTime;
     }
 
-    private void spawnEnemy()
+    private void spawnFormation()
+    {
+        //formation position
+        Vector3 pos = new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f), 0f);
+        GameObject form = Instantiate(formations[Random.Range(0, formations.Length)], pos, Quaternion.identity);
+        //random rotation
+        var euler = form.transform.eulerAngles;
+        euler.z = Random.Range(0.0f, 360.0f);
+        form.transform.eulerAngles = euler;
+
+        Transform[] spawnPoints = form.GetComponentsInChildren<Transform>();
+        Vector2 direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        foreach (Transform point in spawnPoints)
+        {
+            if (point == form.transform) continue;
+            GameObject enemy = baseEnemyPool.GetPooledObject();
+            if(enemy==null) Debug.LogError("Enemy Could Not Spawn");
+
+            enemy.transform.position = point.position;
+            enemy.GetComponent<EnemyExplosion>().resetEnemy();
+            enemy.GetComponent<Mover>().setSpeed(5f);
+            enemy.GetComponent<Mover>().setDirection(direction);
+            enemy.SetActive(true);
+        }
+    }
+
+    private void spawnSingleEnemy()
     {
         GameObject enemy = baseEnemyPool.GetPooledObject();
         if(enemy==null) Debug.LogError("Enemy Could Not Spawn");
